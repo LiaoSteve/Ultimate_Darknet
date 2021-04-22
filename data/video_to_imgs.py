c1ac458574284538ea30dafdb20ce91de5bf8bf5
LiaoSteve <steveliao1688@gmail.com>
2021-04-22 15:19:12 +0800

    minor fix

--- data/video_to_imgs.py
+++ data/video_to_imgs.py
@@ -5,8 +5,8 @@
 import os 
 import cv2
 
-input_path = '/home/ai7210/steve/trash_video/DJI/DJI_9.MP4'
-out_dir    = 'images_DJI_9'
+input_path = 'FPV.avi'
+out_dir    = 'images_FPV'
 
 os.makedirs(out_dir, exist_ok=True)
 cap = cv2.VideoCapture(input_path)
@@ -14,7 +14,7 @@ fps = int(cap.get(5))
 all_frame = int(cap.get(7))
 count = 0
 num = 0
-desired_fps = 0.5 # get image per second
+desired_fps = 2 # get image per second
 out_fps = int(fps/desired_fps)
 
 while True:
@@ -23,7 +23,8 @@ while True:
     if ret:
         if count % out_fps == 0:
             num += 1
-            cv2.imwrite(out_dir+'/out_'+str(num)+'.jpg', img)
+            cv2.imwrite(out_dir+'/'+out_dir+'_'+str(num)+'.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
             #print(f"{count}/{all_frame}")
     else: break
 print(f'- [x] Done: save {num} images')
+
